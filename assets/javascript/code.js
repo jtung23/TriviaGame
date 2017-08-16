f983fb7e0011a8b548301cd82d5c1412ae26dbad
Jonathan Tung <jtung23@gmail.com>
2017-08-16 00:54:11 -0700

    stuck@ click guess event

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -13,18 +13,17 @@ var triviaQuestions = {
 	},
 };
 
-console.log(triviaQuestions.firstQ.Answer[1])
-
 var triviaQArray = [triviaQuestions.firstQ.Q, triviaQuestions.secondQ.Q, triviaQuestions.thirdQ.Q];
 
-console.log(triviaQArray);
 var randomTriviaQuestion = triviaQArray[Math.floor(Math.random() * triviaQArray.length)];
-console.log(randomTriviaQuestion);
 
+// Change question function
+function changeQuestion() {
+
+}
 
 // ON START CLICK, HIDE START BUTTON, MAKE FIRST QUESTION, MAKE 4 GUESSES
 $('#start').click(function() {
-	console.log("yes")
 	$('#question').html(randomTriviaQuestion);
 	$('#start').hide();
 	// make answers
@@ -36,12 +35,21 @@ $('#start').click(function() {
 	var secondButton = $('#answer-buttons').children().eq(1).children().eq(0);
 	var thirdButton = $('#answer-buttons').children().eq(2).children().eq(0);
 	var fourthButton = $('#answer-buttons').children().eq(3).children().eq(0);
-	console.log(firstButton)
 
 	firstButton.text(triviaQuestions.firstQ.Answer[0]).attr('id', 'first-button');
 	secondButton.text(triviaQuestions.firstQ.Answer[1]).attr('id', 'second-button');
 	thirdButton.text(triviaQuestions.firstQ.Answer[2]).attr('id', 'third-button');
 	fourthButton.text(triviaQuestions.firstQ.Answer[3]).attr('id', 'fourth-button');
+	
 })
 
-// FIRST QUESTION /////////////////////
+// FIRST QUESTION ///////////////////// changed the DOM. why can click function work on id
+$('button#second-button').click(function() {
+	alert('yay?');
+})
+// click(function() {
+	// if ($('#question').html(triviaQuestions.firstQ.Q)) {
+	// 	console.log('yes')
+	// } else {
+	// 	console.log('no')
+	// }
