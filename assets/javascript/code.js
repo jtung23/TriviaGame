00b33b234a807d89e8a1a39e4d3fa110b9b46678
Jonathan Tung <jtung23@gmail.com>
2017-08-19 16:43:03 -0700

    got some progress. keep over previous commit

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,23 +1,26 @@
+$('#answer-buttons').hide();
 $(document).ready(function(){
 
 var triviaQuestions = {
 	firstQ: {
-		Q: "First Question?",
-		Answer: ["1Answer One", "Answer Two","Answer Three Correct", "Answer Four"],	
+		Q: "What part of the cow does the Sirloin cut come from?",
+		Answer: ["Flank", "Ribs","Hindquarter", "Shank"],	
 	},
 	secondQ: {
-		Q: "Second Question?",
-		Answer: ["2Answer One", "Answer Two","Answer Three", "Answer Four Correct"],
+		Q: "What is the minimum safe internal temperature (F) of Chicken to eat at?",
+		Answer: ["150", "190","210", "165"],
 	},
 	thirdQ: {
-		Q: "Third Question?",
-		Answer: ["3Answer One Correct", "Answer Two","Answer Three", "Answer Four"],
+		Q: "What is better than a slow cooked smoked brisket?",
+		Answer: ["Nothing", "Pork Ribs","Fried Chicken", "Smoked Salmon"],
 	},
 	fourthQ: {
-		Q: "Fourth Question?",
-		Answer: ["4Answer One", "Answer Two Correct","Answer Three", "Answer Four"],
+		Q: "At what degree of doneness should you NOT eat steak at?",
+		Answer: ["Medium Rare", "Well Done","Rare", "Medium"],
 	},
 };
+
+
 var firstQuestion = triviaQuestions.firstQ.Q	
 var firstQAnswer = triviaQuestions.firstQ.Answer
 
@@ -30,107 +33,162 @@ var thirdQAnswer = triviaQuestions.thirdQ.Answer[0]
 var fourthQuestion = triviaQuestions.fourthQ.Q
 var fourthQAnswer = triviaQuestions.fourthQ.Answer[1]
 
+var button1 = $('#answer1')
+var button2 = $('#answer2')
+var button3 = $('#answer3')
+var button4 = $('#answer4')
+
 var triviaQArray = [triviaQuestions.firstQ.Q, triviaQuestions.secondQ.Q, triviaQuestions.thirdQ.Q];
+var qArrayNum = 0
 
 var randomTriviaQuestion = triviaQArray[Math.floor(Math.random() * triviaQArray.length)];
 var questionId = $('#question').text()
-var time = 30
-function setTimer () {
-	$('#timer').html("Time left:" + time)
-	setInterval(startQuestionTimer, 1000)
-	setTimeout(startQuestionTimer, 1000)
-}
 
+var score = 0
+var badscore = 0
+
+
+// TIMER STUFF ///////////////////////////////////////////
+
+var time = 30
+function resetTimer () {
+	time = 30
+	$('#timer').html("Time Left: " + time)
+}
+function setTimer () {
+	$('#timer').html("Time left: " + time)
+	thirtyIntervalId = setInterval(startQuestionTimer, 1000)
+
+}
 // Change question function
 function startQuestionTimer() {
 	time--
-	setTimeout(startQuestionTimer, 1000)
-	$('#timer').html(time)
+	$('#timer').html("Time left: " + time)
 	console.log(time)
 
-	if (time === 25) {
-		clearInterval()
-		clearTimeout()
+	if (time === 23) {
+		qArrayNum++
+		if (qArrayNum == triviaQArray.length) {
+			console.log('done')
+		}
 		console.log('out of time')
+		clearInterval(thirtyIntervalId)
+		resetTimer()
+		generateButtons()
 	}
-
+	return qArrayNum
 }
+
+// //////////////////////////////////////////////////////////////////////////
 
 
 
 function hideQuestion() {
-	$('#answer-buttons').empty()
+	$('#answer-buttons').hide()
 }
 
-function firstAnswerButtons() {
-	for (var i = 1; i < 5; i++) {
-		var buttons = '<button type=button class="btn btn-default btn-lg btn-block"></button>';
-		$('#answers' + i).append(buttons);
-	}
-	var firstButton = $('#answer-buttons').children().eq(0).children().eq(0);
-	var secondButton = $('#answer-buttons').children().eq(1).children().eq(0);
-	var thirdButton = $('#answer-buttons').children().eq(2).children().eq(0);
-	var fourthButton = $('#answer-buttons').children().eq(3).children().eq(0);
+function generateButtons() {
+	console.log(qArrayNum)
+	$('#answer-buttons').show();
+	startQuestionTimer()
 
-	console.log(firstButton)
-	console.log(firstQAnswer[0])
-	$(firstButton).text(firstQAnswer[0]).attr("id", "button1")
-	console.log(firstButton.text())
-	$(secondButton).text(firstQAnswer[1]).attr("id", "button2")
-	$(thirdButton).text(firstQAnswer[2]).attr("id", "button3")
-	$(fourthButton).text(firstQAnswer[3]).attr("id", "button4")
-
-	$('#button3').click(function() {
-		console.log('first button clicked');
-		hideQuestion();
-		$('#questionanswer').append('<div class=jumbotron>Works!</div>')
-		setTimeout(secondAnswerButtons, 1000 * 3);
-	})
-	$('#button1, #button2, #button4').click(function() {
-		console.log('first wrong button clicked')
-		hideQuestion();
-	})
-
-}
-
-function secondAnswerButtons() {
-	$('#questionanswer').empty();
+	// Question 1///////////////////////////
+	if (qArrayNum == 0) {
 	$('#question').html(firstQuestion);
-		for (var i = 1; i < 5; i++) {
-		var buttons = '<button type=button class="btn btn-default btn-lg btn-block"></button>';
-		$('#answers' + i).append(buttons);
-	}
-	var firstButton = $('#answer-buttons').children().eq(0).children().eq(0);
-	var secondButton = $('#answer-buttons').children().eq(1).children().eq(0);
-	var thirdButton = $('#answer-buttons').children().eq(2).children().eq(0);
-	var fourthButton = $('#answer-buttons').children().eq(3).children().eq(0);
+	$(button1).text(firstQAnswer[0])
+	$(button2).text(firstQAnswer[1])
+	$(button3).text(firstQAnswer[2])
+	$(button4).text(firstQAnswer[3])
 
-	console.log(firstButton)
-	console.log(secondQAnswer[0])
-	$(firstButton).text(secondQAnswer[0]).attr("id", "button1")
-	console.log(secondButton.text())
-	$(secondButton).text(secondQAnswer[1]).attr("id", "button2")
-	$(thirdButton).text(secondQAnswer[2]).attr("id", "button3")
-	$(fourthButton).text(secondQAnswer[3]).attr("id", "button4")
+	$(button3).click(function() {
+		console.log('first button clicked');
+		qArrayNum++
+		score++
+		return qArrayNum++
+		generateButtons();
+	})
+	$("#answer1, #answer2, #answer4").click(function() {
+		console.log('first wrong button clicked')
+		qArrayNum++
+		badscore++
+		return qArrayNum++
+		console.log(qArrayNum);
+		generateButtons()
+	})	
+	}
+
+// Question 2///////////////////////////
+
+	 else if (qArrayNum == 1) {
+	$('#question').html(secondQuestion);
+	$(button1).text(secondQAnswer[0])
+	$(button2).text(secondQAnswer[1])
+	$(button3).text(secondQAnswer[2])
+	$(button4).text(secondQAnswer[3])
 
 	$('#button4').click(function() {
 		console.log('first button clicked');
-		hideQuestion();
-		$('#questionanswer').append('<div class=jumbotron>Works!</div>')
-		setTimeout(secondAnswerButtons, 1000 * 3);
+		qArrayNum++
+		score++
+		return qArrayNum++
+		generateButtons();
 	})
 	$('#button1, #button2, #button3').click(function() {
 		console.log('first wrong button clicked')
 		hideQuestion();
 	})
+	} 
+
+// Question 3///////////////////////////
+
+	else if (qArrayNum == 2) {
+		$('#question').html(thirdQuestion);
+	$(button1).text(thirdQAnswer[0])
+	$(button2).text(thirdQAnswer[1])
+	$(button3).text(thirdQAnswer[2])
+	$(button4).text(thirdQAnswer[3])
+
+	$('#button1').click(function() {
+		console.log('first button clicked');
+		qArrayNum++
+		score++
+		return qArrayNum++
+		generateButtons();
+	})
+	$('#button4, #button2, #button3').click(function() {
+		console.log('first wrong button clicked')
+		hideQuestion();
+	})
+	}
+	else if (qArrayNum == 3) {
+	$('#question').html(fourthQuestion);
+	$(button1).text(fourthQAnswer[0])
+	$(button2).text(fourthQAnswer[1])
+	$(button3).text(fourthQAnswer[2])
+	$(button4).text(fourthQAnswer[3])
+
+	$('#button2').click(function() {
+		console.log('first button clicked');
+		qArrayNum++
+		score++
+		return qArrayNum++
+		generateButtons();
+	})
+	$('#button4, #button1, #button3').click(function() {
+		console.log('first wrong button clicked')
+		hideQuestion();
+	})
+	}
+	else {
+		console.log('game over')
+	} 
 }
 
 // ON START CLICK, HIDE START BUTTON, MAKE FIRST QUESTION, MAKE 4 GUESSES
 $('#start').click(function() {
-	$('#question').html(firstQuestion);
 	$('#start').hide();
-	firstAnswerButtons()
-	startQuestionTimer()
+	setTimer()
+	generateButtons()
 })
 
 
